df4d63d418efeac7108eaf33c3531ae8bae87a96
Seppe De Witte <Seppe.dewitt@gmail.com>
2020-04-20 13:37:47 +0200

    seppe

---
+++ Project-Week-2020/Project-Week-2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Week_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

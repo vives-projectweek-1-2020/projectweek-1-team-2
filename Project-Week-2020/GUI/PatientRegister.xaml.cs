233278d94ffd70593b09c2349d85bcbc99a7ca2b
Joachim De Graeve <degraevejoachim@gmail.com>
2020-04-23 16:34:26 +0200

    acces code textblok alleen meer cijfers

--- Project-Week-2020/GUI/PatientRegister.xaml.cs
+++ Project-Week-2020/GUI/PatientRegister.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Project_Week_2020;
+using System.Text.RegularExpressions;
 
 
 namespace GUI
@@ -134,6 +135,11 @@ public PatientRegister()
         {
             email.Text = "";
             email.Focus();
+        }
+
+        private void accesscode_previewtextinput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
     }
 }
